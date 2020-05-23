be18cd5794df031491ec2a4bf324cbf37d2eb195
Piyush Suthar <32237558+PiyushSuthar@users.noreply.github.com>
2020-05-23 17:45:02 +0530

    Innitial Commit

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ async function onAuthenticated(err){
     } else {
     console.log('Authentication successful.')
 
-    var stream = T.stream('statuses/filter', { track: ['@PiyushSthr'] });
+    var stream = await T.stream('statuses/filter', { track: ['@PiyushSthr'] });
     stream.on('tweet', (eventmsg)=> {
         //getting name and username of the user
         var name = eventmsg.source.name;
@@ -30,7 +30,7 @@ async function onAuthenticated(err){
         var nameID  = tweet.id_str;
         //since twitter blocks tweets of same type so we'll associate a
         //unique number using Math.random() or anything you like
-        nodeHtmlToImage({
+        await nodeHtmlToImage({
             output: './image.png',
             html: '<!DOCTYPE html> <html lang=""><body> <style> body { margin: 0; background: white; color: #323232; font-family: Helvetica neue, roboto; }.outer{ height: 335px; width: 600px; background:linear-gradient(0deg, rgba(0, 0, 0, 0.575), rgba(65, 65, 65, 0.37)), url("https://source.unsplash.com/600x335/?happy.google,coding"); background-color: rgb(23, 23, 36); display: flex; flex-direction: column; align-items: center; justify-content: center; columns: white; } .profile img{ width: 100px; border-radius: 50%; border: 6px solid white ; } .profile{ color: white; } .thanks{ font-family: cursive; color: white; font-size: 30px; } </style> <div class="outer"> <div class="thanks"> <h4>{{text}}</h4> </div> <div class="profile"> <img src="{{profileUrl}}" alt=""><h3>@{{name}}</h3> </div> </div> </body> </html>',
             content: { text:"Thanks for Mention!", name: screenName , profileUrl: imageUrl},
