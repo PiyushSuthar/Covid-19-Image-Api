56386b6aa44f125c7d3202fa117f3b378f9c7f4e
Piyush Suthar <32237558+PiyushSuthar@users.noreply.github.com>
2020-06-10 20:34:55 +0530

    initial Commit

--- bot.js
+++ bot.js
@@ -39,7 +39,7 @@ app.get("/country/:id", (req,res)=>{
    var country = req.params.id;
    fetch(`https://disease.sh/v2/countries/${country}`)
    .then(res => res.json())
-   .then(json => {fs.closeSync(fs.openSync(filename,"w"));getIt(json, "false", json.country, `country/${country}.png`).then(res.sendFile(`${country}.png`, options, (err)=>{
+   .then(json => {fs.closeSync(fs.openSync(`country/${country}.png`,"w"));getIt(json, "false", json.country, `country/${country}.png`).then(res.sendFile(`${country}.png`, options, (err)=>{
       console.log(err)
    }))})
    
